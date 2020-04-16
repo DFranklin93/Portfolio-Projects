62899c0299d82c1c62f3d2609d0db1f2b039de13
Don Franklin <don2226@live.com>
2020-04-16 10:44:00 -0500

    Updates

---
+++ precache-manifest.e2b46d3504450158651783f1ff786244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b98b09898afb7c03bf58c16e2bf7e0",
+    "url": "/hat-shop/index.html"
+  },
+  {
+    "revision": "4a2e9cdc165fcc08d84f",
+    "url": "/hat-shop/static/css/main.e79f7816.chunk.css"
+  },
+  {
+    "revision": "0090d62b2d082f503f19",
+    "url": "/hat-shop/static/js/2.18c29513.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/hat-shop/static/js/2.18c29513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2e9cdc165fcc08d84f",
+    "url": "/hat-shop/static/js/main.96c248e7.chunk.js"
+  },
+  {
+    "revision": "a34a0b5aee2ea476ebb1",
+    "url": "/hat-shop/static/js/runtime-main.1a2cb605.js"
+  }
+]);
